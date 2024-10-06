2bd41aeb148e59f5a22a5b523605ad27cc764835
Kanishka Misra <menogetusername@gmail.com>
2024-10-06 13:58:18 -0500

    control exps on gemma

---
+++ scripts/control-experiments2.sh
@@ -0,0 +1,68 @@
+# declare -a models=(google/Gemma-2-9B-it google/Gemma-2-2B-it meta-llama/Meta-Llama-3-8B-Instruct)
+declare -a triples=(spose_prototype)
+
+# TRANSFORMERS_CACHE="/home/shared/km_cache"
+device="cuda:1"
+
+for triples in ${triples[@]}; do
+    if [[ $triples == "sense_based_ns" ]]; then
+        triples_path="data/things/stimuli-pairs/things-inheritance-sense_based_sim-pairs.csv"
+        save_dir="data/things/results/things-sense_based-ns"
+    elif [[ $triples == "spose_prototype" ]]; then
+        triples_path="data/things/stimuli-pairs/things-inheritance-SPOSE_prototype_sim-pairs.csv"
+        save_dir="data/things/results/things-SPOSE_prototype-ns"
+    fi
+
+    python src/behavioral_eval.py \
+        --batch_size 16 \
+        --num_examples -1 \
+        --device $device \
+        --model google/Gemma-2-9B-it \
+        --triples_path $triples_path \
+        --save \
+        --save_dir ${save_dir}_multi-property \
+        --qa_format \
+        --prompt_template variation-qa-2 \
+        --chat_format \
+        --multi_property
+
+    python src/behavioral_eval.py \
+        --batch_size 16 \
+        --num_examples -1 \
+        --device $device \
+        --model google/Gemma-2-9B-it \
+        --triples_path $triples_path \
+        --save \
+        --save_dir ${save_dir}_multi-property_prop-contrast \
+        --qa_format \
+        --prompt_template variation-qa-2 \
+        --chat_format \
+        --multi_property \
+        --prop_contrast
+
+    python src/behavioral_eval.py \
+        --batch_size 16 \
+        --num_examples -1 \
+        --device $device \
+        --model google/Gemma-2-2B-it \
+        --triples_path $triples_path \
+        --save \
+        --save_dir ${save_dir}_multi-property \
+        --qa_format \
+        --prompt_template variation-qa-1 \
+        --multi_property
+
+    python src/behavioral_eval.py \
+        --batch_size 16 \
+        --num_examples -1 \
+        --device $device \
+        --model google/Gemma-2-2B-it \
+        --triples_path $triples_path \
+        --save \
+        --save_dir ${save_dir}_multi-property_prop-contrast \
+        --qa_format \
+        --prompt_template variation-qa-1 \
+        --multi_property \
+        --prop_contrast
+done
+
